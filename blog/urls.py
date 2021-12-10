a60fa0a8f8e5653c2335d0ae539baeffd15d8705
Yasser-ui <yasser.benyoussef@isigk.u-kairouan.tn>
2021-12-10 23:08:58 +0100

    commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from django.urls import path
-from . import views
+from django.urls import path, re_path
+from django.contrib import admin
 
-URLPattern = [
+from blog import views
+
+URLPatterns = [
     path('', views.post_list, name='post_list'),
 ]
