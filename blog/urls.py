d679610bfbd2fa624011362ff5de93684eae3884
Yasser-ui <yasser.benyoussef@isigk.u-kairouan.tn>
2021-12-12 12:02:58 +0100

    Modification du HTML du site

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,5 @@
 from django.urls import path
-from django.urls import path, re_path
-from django.contrib import admin
-
-from blog import views
+from . import views
 
 URLPatterns = [
     path('', views.post_list, name='post_list'),
